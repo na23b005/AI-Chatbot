86e4189452273dbd520ff4f48dd90abfa239a923
vijaykrishna483-cms <vijay762005@gmail..com>
2025-08-08 22:18:21 +0530

    DG

--- ai-playground-frontend/src/App.jsx
+++ ai-playground-frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/home.jsx'
+import Home from './pages/Home.jsx'
 import ChatPage from './pages/ChatPage.jsx'
-import Navbar from './Components/Navbar.jsx'
+
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      {/* Simple Navbar */}
-      {/* <Navbar /> */}
+     
 
       <Routes>
         <Route path="/" element={<Home />} />
